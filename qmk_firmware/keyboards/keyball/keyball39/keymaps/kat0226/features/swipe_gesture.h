dc0a4244dfe44a8d6d1ac6d6f9031f51104340a5
kat0226 <40346684+kat0226@users.noreply.github.com>
2026-02-16 21:50:53 +0900

    Update swipe_gesture.h 古いバージョンの記載を修正 from Gemini

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kat0226/features/swipe_gesture.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kat0226/features/swipe_gesture.h
@@ -12,14 +12,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * -------------------------------------------------------------------
- *
- * 参考文献：
- * - 以下のリソースを参考にしました：
- *   https://www.reddit.com/r/ploopy/comments/pbmrh3/qmk_options_on_ploopy_mouse_gestures/
- *   https://github.com/RobertCurry0216/qmk_firmware/blob/59f83aac8fa06010c3a1a0a53fcd453d96ce2f80/keyboards/ploopyco/trackball/keymaps/robertcurry0216/keymap.c
- *
  */
 
 int16_t current_keycode;
@@ -49,7 +41,7 @@ void process_swipe_gesture(int16_t x, int16_t y) {
 
     // 次のモニターへ移動
     if (my_abs(x) > my_abs(y)) {
-      register_code(KC_LCTRL);
+      register_code(KC_LCTL);
 
       if (x < 0) {
         // swipe left:
@@ -60,13 +52,13 @@ void process_swipe_gesture(int16_t x, int16_t y) {
       }
     }
     unregister_code(KC_LCMD);
-    unregister_code(KC_LCTRL);
+    unregister_code(KC_LCTL);
 
     register_code(KC_LALT);
   }
 
   // shift
-  if (current_keycode == SFT_T(KC_LANG2)) {
+  if (current_keycode == SFT_T(KC_LNG2)) {
     unregister_code(KC_LALT);
     register_code(KC_LCMD);
 
@@ -84,10 +76,10 @@ void process_swipe_gesture(int16_t x, int16_t y) {
     if (my_abs(x) > my_abs(y)) {
       if (x < 0) {
         // swipe left: 戻る
-        tap_code(KC_LBRACKET);
+        tap_code(KC_LBRC);
       } else {
         // swipe right: 進む
-        tap_code(KC_RBRACKET);
+        tap_code(KC_RBRC);
       }
     }
     unregister_code(KC_LCMD);
@@ -97,7 +89,7 @@ void process_swipe_gesture(int16_t x, int16_t y) {
   // CMD
   if (current_keycode == CMD_T(KC_SPACE)) {
     unregister_code(KC_LCMD);
-    register_code(KC_LCTRL);
+    register_code(KC_LCTL);
 
     // ウインドウのサイズ変更と移動（BetterTouchToolで設定）
     if (my_abs(x) < my_abs(y)) {
@@ -120,117 +112,7 @@ void process_swipe_gesture(int16_t x, int16_t y) {
       }
     }
 
-    unregister_code(KC_LCTRL);
+    unregister_code(KC_LCTL);
     register_code(KC_LCMD);
   }
-
-  // alt
-  // if (current_keycode == KC_LALT)
-  // {
-  //   if (my_abs(x) > my_abs(y))
-  //   { // ウインドウのサイズ変更と移動
-  //     register_code(KC_LCMD);
-
-  //     if (x > 0)
-  //     { // swipe right
-  //       tap_code(KC_6);
-  //     }
-  //     else
-  //     { // swipe left
-  //       tap_code(KC_4);
-  //     }
-  //   }
-
-  //   if (my_abs(x) < my_abs(y))
-  //   { // ウインドウのサイズ変更と移動s
-  //     register_code(KC_LCMD);
-
-  //     if (y > 0)
-  //     { // swipe down
-  //       tap_code(KC_2);
-  //     }
-  //     else
-  //     { // swipe up
-  //       tap_code(KC_8);
-  //     }
-  //   }
-  // }
-
-  // alt
-  // if (current_keycode == KC_LALT)
-  // { //
-  //   unregister_code(KC_LALT);
-  //   // register_code(KC_LCMD);
-  //   register_code(KC_LCTL);
-
-  //   if (my_abs(x) > my_abs(y))
-  //   {
-  //     // is_repeat = false;
-  //     if (x > 0)
-  //     { // swipe right
-  //       // register_code16(HYPR(KC_RIGHT));
-  //       // tap_code(KC_RBRACKET);
-  //       tap_code(KC_LEFT);
-  //       // unregister_code(KC_LCMD);
-  //     }
-  //     else
-  //     { // swipe left
-  //       tap_code(KC_RIGHT);
-  //       // register_code16(HYPR(KC_LEFT));
-  //     }
-  //   }
-
-  //   if (my_abs(x) < my_abs(y))
-  //   {
-  //     if (y > 0)
-  //     { // swipe down
-  //       // register_code(KC_LCMD);
-  //       // register_code(KC_LCTL);
-  //       tap_code(KC_DOWN);
-  //       // unregister_code(KC_LCMD);
-  //       // unregister_code(KC_LCTL);
-  //     }
-  //     else
-  //     { // swipe up
-  //       // register_code(KC_LCMD);
-  //       // register_code(KC_LCTL);
-  //       tap_code(KC_UP);
-  //       // unregister_code(KC_LCMD);
-  //       // unregister_code(KC_LCTL);
-  //     }
-  //     // is_repeat = true;
-  //   }
-
-  //   // unregister_code(KC_LCMD);
-  //   unregister_code(KC_LCTL);
-  // }
-
-  // layer2 & a
-  // if (current_keycode == LT(2, KC_A))
-  // {
-  //   register_code(KC_LCMD);
-
-  //   if (my_abs(x) > my_abs(y))
-  //   { // 英かな切り替え
-  //     if (x > 0)
-  //     { // swipe right
-  //       tap_code(KC_RBRACKET);
-  //     }
-  //     else
-  //     { // swipe left
-  //       tap_code(KC_LBRACKET);
-  //     }
-  //   }
-
-  //   if (my_abs(x) < my_abs(y))
-  //   { // 拡大と縮小
-  //     if (y > 0)
-  //     { // swipe down
-  //       tap_code(KC_2);
-  //     }
-  //     else
-  //     { // swipe up
-  //       tap_code(KC_1);
-  //     }
-  //   }
 }
